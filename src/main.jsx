2075a2ca1df0cdd6787cb609dbd9a99543c6759c
Simon-157 <boatengsimonjnr157@gmail.com>
2023-12-10 10:35:21 +0000

    feat: added protected routes for admin private routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -15,21 +15,9 @@ import SoundEffectPlayer from './hotline-room-engine/room/sound/SoundEffectPlaye
 import { createBrowserHistory } from 'history';
 import { BrowserRouter } from 'react-router-dom';
 
-const ReactQueryDevtoolsProduction = React.lazy(() =>
-  import('react-query/devtools/development').then(d => ({
-    default: d.ReactQueryDevtools,
-  }))
-)
-
 
 const history = createBrowserHistory();
 const queryClient = new QueryClient();
-  const [showDevtools, setShowDevtools] = useState(false)
-
-  React.useEffect(() => {
-    // @ts-ignore
-    window.toggleDevtools = () => setShowDevtools(old => !old)
-  }, [])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -46,11 +34,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         {/* </WebSocketProvider> */}
         <Toaster position="bottom-center" reverseOrder={true} />
         <ReactQueryDevtools initialIsOpen={false} />
-         {showDevtools ? (
-        <React.Suspense fallback={null}>
-          <ReactQueryDevtoolsProduction />
-        </React.Suspense>
-      ) : null}
       </UserProvider>
     </QueryClientProvider>
   </React.StrictMode>,
