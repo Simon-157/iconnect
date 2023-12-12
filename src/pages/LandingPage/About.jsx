e7b9b61aeb9fb57bc3cda7dc05e12610badef761
Simon-157 <boatengsimonjnr157@gmail.com>
2023-12-12 02:14:40 +0000

    styles:responsive styles

--- src/pages/LandingPage/About.jsx
+++ src/pages/LandingPage/About.jsx
@@ -1,24 +1,27 @@
-/* eslint-disable react/prop-types */
-import { Link } from 'react-router-dom'
-// components
+import { Link } from 'react-router-dom';
 import { AppLayout } from "../../components/ui/AppLayout";
 import AppSideBar from '../../components/common/AppSideBar';
 
 const About = ({ setSelectedLink }) => {
-
   const handleLinkClick = (link) => {
     setSelectedLink(link);
-    };
+  };
 
   return (
-    <>
-          <div className='w-full pt-[20px]'>
-          <p className='font-[Inter] font-normal text-[20px] leading-10 tracking-wide text-app-white'>
-          <span className='font-medium'>ASHESI iCONNECT</span> aims to enhance communication and issue resolution within the Ashesi University community. This application provides a platform for students, faculty, and staff to report various issues, such as maintenance requests, safety concerns, academic inquiries, and more, to the appropriate departments or categories for efficient and timely resolution. <Link to="/about" className="text-[#00000085] dark:text-[#ffffffac]" onClick={()=>handleLinkClick('about')} >Read more...</Link>
+    <AppLayout>
+      <div className='flex flex-col lg:flex-row lg:space-x-6'>
+        <AppSideBar />
+        <div className='w-full lg:w-3/4 pt-6 lg:pt-0'>
+          <p className='font-inter font-normal text-base lg:text-lg leading-7 lg:leading-8 tracking-wide text-app-white'>
+            <span className='font-medium'>ASHESI iCONNECT</span> aims to enhance communication and issue resolution within the Ashesi University community. This application provides a platform for students, faculty, and staff to report various issues, such as maintenance requests, safety concerns, academic inquiries, and more, to the appropriate departments or categories for efficient and timely resolution.{' '}
+            <Link to="/about" className="text-[#00000085] dark:text-[#ffffffac]" onClick={() => handleLinkClick('about')}>
+              Read more...
+            </Link>
           </p>
         </div>
-    </>
-  )
-}
+      </div>
+    </AppLayout>
+  );
+};
 
-export default About
+export default About;
